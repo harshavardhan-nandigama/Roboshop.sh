3d9d0764425fb5bce35cc6cfef106d8d62bbdf61
unknown <harha@gmail.com>
2025-05-24 11:24:03 +0530

    rr

--- shipping.sh
+++ shipping.sh
@@ -5,7 +5,7 @@ app_name=shipping
 
 check_root
 echo "Please enter root password to setup"
-read -s MYSQL_ROOT_PASSWORD
+read -s harsha123
 
 app_setup
 maven_setup
@@ -14,12 +14,12 @@ systemd_setup
 dnf install mysql -y  &>>$LOG_FILE
 VALIDATE $? "Install MySQL"
 
-mysql -h mysql.harshavn24.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
+mysql -h mysql.harshavn24.site -u root -p$harsha123 -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    mysql -h mysql.harshavn24.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h mysql.harshavn24.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
-    mysql -h mysql.harshavn24.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+    mysql -h mysql.harshavn24.site -uroot -p$harsha123 < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.harshavn24.site -uroot -p$harsha123 < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h mysql.harshavn24.site -uroot -p$harsha123 < /app/db/master-data.sql &>>$LOG_FILE
     VALIDATE $? "Loading data into MySQL"
 else
     echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
